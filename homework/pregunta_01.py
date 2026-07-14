15864ea081e666df387a53fdd6640f47c41c153d
Catalina Arango <75914408+Cataarangod@users.noreply.github.com>
2026-07-14 00:42:20 -0500

    done

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -6,13 +6,80 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 
 
 def pregunta_01():
-    """
-    Siga las instrucciones del video https://youtu.be/qVdwpxG_JpE para
-    generar el archivo `files/plots/news.png`.
+    import os
+    import pandas as pd
+    import matplotlib.pyplot as plt
 
-    Un ejemplo de la grafica final esta ubicado en la raíz de
-    este repo.
+    df = pd.read_csv("files/input/news.csv", index_col=0)
 
-    El gráfico debe salvarse al archivo `files/plots/news.png`.
+    colors = {
+        "Television": "dimgrey",
+        "Newspaper": "grey",
+        "Internet": "tab:blue",
+        "Radio": "lightgrey",
+    }
 
-    """
+    zorder = {
+        "Television": 1,
+        "Newspaper": 1,
+        "Internet": 2,
+        "Radio": 1,
+    }
+
+    linewidth = {
+        "Television": 1,
+        "Newspaper": 1,
+        "Internet": 4,
+        "Radio": 1,
+    }
+
+    plt.figure()
+
+    for col in df.columns:
+        plt.plot(
+            df[col],
+            label=col,
+            color=colors[col],
+            zorder=zorder[col],
+            linewidth=linewidth[col],
+        )
+
+    plt.title("People get news", fontsize=16)
+
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+    plt.gca().axes.get_yaxis().set_visible(False)
+
+    first_year = df.index[0]
+    for col in df.columns:
+        plt.scatter(first_year, df[col].iloc[0], color=colors[col], zorder=zorder[col])
+
+    last_year = df.index[-1]
+    for col in df.columns:
+        plt.scatter(last_year, df[col].iloc[-1], color=colors[col], zorder=zorder[col])
+
+    for col in df.columns:
+        plt.text(
+            first_year - 0.2,
+            df[col].iloc[0],
+            f"{col} {df[col].iloc[0]}%",
+            ha="right",
+            va="center",
+            color=colors[col],
+        )
+
+    for col in df.columns:
+        plt.text(
+            last_year + 0.2,
+            df[col].iloc[-1],
+            f"{df[col].iloc[-1]}%",
+            ha="left",
+            va="center",
+            color=colors[col],
+        )
+
+    plt.xticks(ticks=df.index, labels=[str(y) for y in df.index], ha="center")
+
+    os.makedirs("files/plots", exist_ok=True)
+    plt.tight_layout()
+    plt.savefig("files/plots/news.png")
